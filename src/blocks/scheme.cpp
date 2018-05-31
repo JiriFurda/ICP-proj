a9f144af9ad9473cb1d76849ae6427578edab7f2
Jiří Furda <Furdys@users.noreply.github.com>
2018-05-31 12:12:56 +0200

    Fixed TRUE being represented by 0, added examples

--- src/blocks/scheme.cpp
+++ src/blocks/scheme.cpp
@@ -198,9 +198,9 @@ Block* Scheme::searchUserDependentBlocks()  // Returns block to store pointer to
                                 QString("Select bool value (port #"+QString::number(portNum)+"):"), items, 0, false, &ok);
 
                             if(item == "True")
-                                value = 0;
-                            else
                                 value = 1;
+                            else
+                                value = 0;
                         }
                         else
                         {
